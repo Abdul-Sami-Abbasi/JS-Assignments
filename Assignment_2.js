1d536b86d3fb2c7e0fcc34f81e75a9a724bac875
Abdul-Sami-Abbasi <163078170+Abdul-Sami-Abbasi@users.noreply.github.com>
2024-08-21 11:42:46 +0500

    Add files via upload

---
+++ Assignment_2.js
@@ -0,0 +1,33 @@
+var username;
+
+var myname = "Abdul Sami Abbasi";
+
+var message = "Hello World";
+
+alert(message);
+
+alert("myname");
+
+var age = 18;
+
+alert(age +" years old");
+
+var certificate = "Certified Web and Applications Developer";
+
+alert(certificate);
+
+var p = "PIZZA";
+
+alert(p + "\nPIZZ\nPIZ\nPI\nP");
+
+var email = "abdulsamiabbasi443@gmail.com";
+var text = "my email address is";
+alert(text + email);
+
+var book = "A Smarter Way To Learn JAvascript";
+var btext = "I am Learning From ";
+alert(btext + book);
+
+document.body.innerHTML = "yeh! i can write html content through HTML";
+
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
