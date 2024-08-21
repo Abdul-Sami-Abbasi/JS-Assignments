3dd81c396526a2ef596ff8c4d7dee30b0bfeeb8b
Abdul-Sami-Abbasi <163078170+Abdul-Sami-Abbasi@users.noreply.github.com>
2024-08-21 09:16:08 +0500

    Add files via upload

---
+++ Assignment_1(Alerts).js
@@ -0,0 +1,7 @@
+alert("Please! Enter a valid password");
+
+alert("Welcome to js world...\nHappy Coding!");
+
+alert("Welcome to js world");
+
+alert("Happy Coding!\n▢ Prevent this page from creating additional dialogues");
