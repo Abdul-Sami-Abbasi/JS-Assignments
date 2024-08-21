62cf8961197e1c3947a379465bb20db662a091fb
Abdul-Sami-Abbasi <163078170+Abdul-Sami-Abbasi@users.noreply.github.com>
2024-08-21 12:33:14 +0500

    Add files via upload

---
+++ Assignment_4.js
@@ -0,0 +1,17 @@
+var variable1, variable2, variable3;
+
+
+// Declare 5 legal & 5 illegal variable names.
+var v1, v2, v3, v4, v5;
+
+var 1v, .2v, *v3, @v4, v@5; 
+
+document.body.innerHTML = "<b>Rules for naming js variables</b>";
+
+document.body.innerHTML = "variables only contains numbers, letters, $ and _. For example $my_1stVariable";
+
+document.body.innerHTML = "Variables must begin with a letter, $ or _. For example $name, _name or name";
+
+document.body.innerHTML = "Variable names are case sensitive";
+
+document.body.innerHTML = "Variable names should not be JS keywords";
