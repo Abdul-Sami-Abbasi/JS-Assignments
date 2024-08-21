febca0f7a9ec1206e4913d75ffa2651851e51244
Abdul-Sami-Abbasi <163078170+Abdul-Sami-Abbasi@users.noreply.github.com>
2024-08-21 15:54:39 +0500

    Add files via upload

---
+++ Assignment_5.js
@@ -0,0 +1,20 @@
+var num1 = 3;
+var num2 = 5;
+var num3 = num1 + num2;
+document.body.innerHTML = num1 + " + " + num2 +" = " + num3;
+
+var num1 = 3;
+var num2 = 5;
+var num3 = num1 - num2;
+document.body.innerHTML = num1 + " - " + num2 +" = " + num3;
+
+var num1 = 3;
+var num2 = 5;
+var num3 = num1 * num2;
+document.body.innerHTML = num1 + " * " + num2 +" = " + num3;
+
+var num1 = 3;
+var num2 = 5;
+var num3 = num1 / num2;
+document.body.innerHTML = num1 + " / " + num2 +" = " + num3;
+
