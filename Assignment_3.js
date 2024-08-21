85bdc283766e7d2f20c1f883a7dd7b39d90a9bb5
Abdul-Sami-Abbasi <163078170+Abdul-Sami-Abbasi@users.noreply.github.com>
2024-08-21 12:11:42 +0500

    Add files via upload

---
+++ Assignment_3.js
@@ -0,0 +1,12 @@
+var age = 21;
+alert("i am "+ age + " years old");
+
+var birthyear = 2003;
+document.body.innerHTML = "My Bith Year is "+ birthyear +"\n data type of my decleared variale is Number";
+
+var custommerName = "john Doe";
+var items = "t-shirts";
+var num = 4;
+var storeName = "xyzClothing.com";
+
+document.body.innerHTML = custommerName + " brought " + num +" "+ items +" from "+ storeName;
