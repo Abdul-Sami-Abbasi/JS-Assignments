e71bf7a98c817b277e0057a76d60b08e78e6cdf9
Abdul-Sami-Abbasi <163078170+Abdul-Sami-Abbasi@users.noreply.github.com>
2024-09-11 17:22:42 +0500

    Add files via upload
    initial commit

---
+++ exercise1-7.js
@@ -0,0 +1,105 @@
+// exercise: 1
+alert("Please! Enter a valid password");
+
+alert("Welcome to js world...\nHappy Coding!");
+
+alert("Welcome to js world");
+
+alert("Happy Coding!\n▢ Prevent this page from creating additional dialogues");
+
+
+
+// exercise: 2
+var username;
+
+var myname = "Abdul Sami Abbasi";
+
+var message = "Hello World";
+
+alert(message);
+
+alert("myname");
+
+var age = 18;
+
+alert(age +" years old");
+
+var certificate = "Certified Web and Applications Developer";
+
+alert(certificate);
+
+var p = "PIZZA";
+
+alert(p + "\nPIZZ\nPIZ\nPI\nP");
+
+var email = "abdulsamiabbasi443@gmail.com";
+var text = "my email address is";
+alert(text + email);
+
+var book = "A Smarter Way To Learn JAvascript";
+var btext = "I am Learning From ";
+alert(btext + book);
+
+document.body.innerHTML = "yeh! i can write html content through HTML";
+
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+
+
+// exercise: 3
+var age = 21;
+alert("i am "+ age + " years old");
+
+var birthyear = 2003;
+document.body.innerHTML = "My Bith Year is "+ birthyear +"\n data type of my decleared variale is Number";
+
+var custommerName = "john Doe";
+var items = "t-shirts";
+var num = 4;
+var storeName = "xyzClothing.com";
+
+document.body.innerHTML = custommerName + " brought " + num +" "+ items +" from "+ storeName;
+
+
+
+// exercise: 4
+var variable1, variable2, variable3;
+
+
+// Declare 5 legal & 5 illegal variable names.
+var v1, v2, v3, v4, v5;
+
+var 1v, .2v, *v3, @v4, v@5; //illegal
+
+document.body.innerHTML = "<b>Rules for naming js variables</b>";
+
+document.body.innerHTML = "variables only contains numbers, letters, $ and _. For example $my_1stVariable";
+
+document.body.innerHTML = "Variables must begin with a letter, $ or _. For example $name, _name or name";
+
+document.body.innerHTML = "Variable names are case sensitive";
+
+document.body.innerHTML = "Variable names should not be JS keywords";
+
+
+
+// exercise: 5
+var num1 = 3;
+var num2 = 5;
+var num3 = num1 + num2;
+document.body.innerHTML = num1 + " + " + num2 +" = " + num3;
+
+var num1 = 3;
+var num2 = 5;
+var num3 = num1 - num2;
+document.body.innerHTML = num1 + " - " + num2 +" = " + num3;
+
+var num1 = 3;
+var num2 = 5;
+var num3 = num1 * num2;
+document.body.innerHTML = num1 + " * " + num2 +" = " + num3;
+
+var num1 = 3;
+var num2 = 5;
+var num3 = num1 / num2;
+document.body.innerHTML = num1 + " / " + num2 +" = " + num3;
