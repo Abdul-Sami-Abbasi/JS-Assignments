507e98960ca5f992080562cc8885c69296ca3c56
Abdul Sami Abbasi <163078170+Abdul-Sami-Abbasi@users.noreply.github.com>
2025-05-05 18:15:01 +0500

    Add files via upload

---
+++ project_one/script.js
@@ -0,0 +1,20 @@
+const emoji = document.querySelector('#emoji');
+
+const emojis = [
+    "😀",
+    "😃",
+    "😄",
+    "😁",
+    "😆",
+    "😅",
+    "😂",
+    "🙂",
+    "😉",
+    "😀",
+    "😇",
+    "🥰"
+];
+
+emoji.addEventListener('mouseover', () => {
+    emoji.innerHTML = emojis[Math.floor(Math.random() * emojis.length)]
+})
